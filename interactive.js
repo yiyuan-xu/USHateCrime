001483bf4ec290c401dd589c1a47a8a884e37f1b
Danny An <danny.an@sugarinc.cn>
2021-04-13 03:22:07 +0800

    all update and merge

--- interactive.js
+++ interactive.js
@@ -2,7 +2,7 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
 width = 600 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("#my_dataviz").append("svg")
+var svg = d3.select("#interactive_area").append("svg")
 .attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom)
 .append("g")
 .attr("transform","translate(" + margin.left + "," + margin.top + ")");
